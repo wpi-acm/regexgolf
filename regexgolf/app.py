0662505763b77d802a1aa13d2800ca13cac405e3
Ian Adam Naval <ianonavy@gmail.com>
2014-10-04 17:21:55 -0400

    Fix exception thrown on invalid login

--- regexgolf/app.py
+++ regexgolf/app.py
@@ -130,14 +130,16 @@ def ldap_fetch(uid=None, name=None, passwd=None):
             encryption='ssl')
         res = conn.search('uid={0}'.format(name), base_dn=config.BASE_DN)
         dn = config.BIND_DN_FORMAT.format(res[0]['wpieduPersonUUID'][0])
-        conn = simpleldap.Connection(
-            config.LDAP_SERVER,
-            port=config.LDAP_PORT,
-            require_cert=False,
-            dn=dn, password=passwd,
-            encryption='ssl')
-        if conn:
-            result = res
+        try:
+           conn2 = simpleldap.Connection(
+                config.LDAP_SERVER,
+                port=config.LDAP_PORT,
+                require_cert=False,
+                dn=dn, password=passwd,
+                encryption='ssl')
+            result = conn.search('uid={0}'.format(name), base_dn=config.BASE_DN)
+        except:
+            return None
     else:
         conn = simpleldap.Connection(config.LDAP_SERVER)
         result = conn.search(
@@ -145,8 +147,9 @@ def ldap_fetch(uid=None, name=None, passwd=None):
             base_dn=config.BASE_DN)
 
     if result:
+        print result
         return {
-            'name': result[0]['givenName'][0],
+            'name': result[0]['gecos'][0].split(' ')[0],
             'uid': result[0]['uid'][0],
             'id': unicode(result[0]['uidNumber'][0]),
             'gid': int(result[0]['gidNumber'][0])
